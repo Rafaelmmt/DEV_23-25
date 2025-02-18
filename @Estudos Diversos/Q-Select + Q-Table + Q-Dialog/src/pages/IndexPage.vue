944e783bf942e962e953deca4e5fd63974e258d5
Rafaelmmt <144057595+Rafaelmmt@users.noreply.github.com>
2025-02-18 01:42:13 -0300

    18/02/25

--- @Estudos Diversos/Q-Select + Q-Table + Q-Dialog/src/pages/IndexPage.vue
+++ @Estudos Diversos/Q-Select + Q-Table + Q-Dialog/src/pages/IndexPage.vue
@@ -1,11 +1,19 @@
 <template>
   <q-page class="flex flex-center">
     <CompQSelect /> 
-    <CompQTQD />   
+    <CompQTQD />
+    <q-btn @click="goTo('/teste02')">Teste02</q-btn>   
   </q-page>
 </template>
 
 <script setup>
 import CompQSelect from 'src/components/CompQSelect.vue'
-import CompQTQD from 'src/components/CompQTQD.vue';
+import CompQTQD from 'src/components/CompQTQD.vue'
+
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const goTo = (rota) => {
+  router.push(rota)
+}
 </script>

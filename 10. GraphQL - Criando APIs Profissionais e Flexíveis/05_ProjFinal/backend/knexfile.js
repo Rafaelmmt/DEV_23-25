181426eac2b46b253df18f3e16e5d12ed0c3f4ef
Rafaelmmt <144057595+Rafaelmmt@users.noreply.github.com>
2025-03-25 18:09:47 -0300

    25/03/2025

--- 10. GraphQL - Criando APIs Profissionais e Flexíveis/05_ProjFinal/backend/knexfile.js
+++ 10. GraphQL - Criando APIs Profissionais e Flexíveis/05_ProjFinal/backend/knexfile.js
@@ -1,22 +1,18 @@
-import dotenv from 'dotenv'
-dotenv.config()
+require('dotenv').config()
 
-export default {
-  client: 'postgresql',
+module.exports = {
+  client: 'mysql',
   connection: {
     host: process.env.APP_DB_HOST,
     port: process.env.APP_DB_PORT,
     database: process.env.APP_DB_NAME,
     user: process.env.APP_DB_USER,
-    password: process.env.APP_DB_PASSWORD
   },
   pool: {
     min: 2,
     max: 10
   },
   migrations: {
-    directory: './src/migrations',
-    esm: true,
     tableName: 'knex_migrations'
   }
-}
+};
